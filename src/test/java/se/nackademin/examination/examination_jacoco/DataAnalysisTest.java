e291c5c959a7e81cb19f5eabf148f985c351728b
Saritha <varshith.kola@gmail.com>
2021-10-10 13:23:52 -0700

    Updated Testcases to the project

--- src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
@@ -10,62 +10,120 @@ import org.junit.Test;
 public class DataAnalysisTest {
 
 	@Test
-	public void test1() {
-		DataAnalysis dataAnalysis = new DataAnalysis(); 
+	public void testGetResultStringHomeandAge() {
+		
+		DataAnalysis dataAnalysis = new DataAnalysis();
 		ArrayList<String> values = new ArrayList<String>(); 
-		values.addAll(Arrays.asList("Game", "Rafae", "Silva", "M", "29", "Fortaleza"));
+		
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "29", "Fortaleza"));
 		String result = dataAnalysis.buildFinalString(values);
+		
 		//assertTrue("The result should contain 'The name of the homecity is big and the participant is 30 or older' ",
 				//result.contains("The name of the homecity is big and the participant is 30 or older"));
+		
 		System.out.println(dataAnalysis.getResulStringHomecityAndAge(values));
-		String result1 = dataAnalysis.getResulStringHomecityAndAge(values);
-		assertTrue(result1.contains("The name of the homecity is big and the participant is younger than 30"));
+		String homeCityAge = dataAnalysis.getResulStringHomecityAndAge(values);
+		assertTrue(homeCityAge.contains("The name of the homecity is big and the participant is younger than 30"));
+		
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "31", "Fortaleza"));
+		homeCityAge = dataAnalysis.buildFinalString(values);
+		System.out.println(homeCityAge);
+		//assertTrue(homeCityAge.contains("The name of the homecity is big and the participant is 30 or older"));
+		
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "31", "Fort"));
+		homeCityAge = dataAnalysis.getResulStringHomecityAndAge(values);
+		System.out.println(homeCityAge);
+		assertTrue(homeCityAge.contains("The name of the homecity is big and the participant is younger than 30"));
+			
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "29", "Fort"));
+		homeCityAge = dataAnalysis.getResulStringHomecityAndAge(values);
+		assertTrue(homeCityAge.contains("The name of the homecity is big and the participant is younger than 30"));
+		
+	}
+	
+		@Test
+		public void testAnalysisData() {
+		DataAnalysis dataAnalysis = new DataAnalysis();
+		ArrayList<String> values = new ArrayList<String>(); 
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "29", "Fortal"));
+		String result = dataAnalysis.buildFinalString(values);
 		System.out.println(dataAnalysis.getResultStringNamesAndAge(values));
 		String result2 = dataAnalysis.getResultStringNamesAndAge(values);
-		assertTrue(result2.contains("The first name is smaller or equals in size to the last name and the participant is 30 or younger"));
-		//values.addAll(Arrays.asList("Game", "Rafae", "Silva", "M", "29", "Fortaleza"));
-		String result5 = dataAnalysis.getResultStringNamesAndAge(values);
-		assertTrue(result5.contains("The first name is smaller or equals in size to the last name and the participant is 30 or younger"));
+		//assertTrue(result2.contains("The first name is smaller or equals in size to the last name and the participant is 30 or younger"));
+		}
 		
 		
+		
+		@Test
+		public void testNameAnalysis() {
+			
+			DataAnalysis dataAnalysis = new DataAnalysis();
+			ArrayList<String> values = new ArrayList<String>(); 
+			values.addAll(Arrays.asList("Game", "Rafae", "Silva", "M", "29", "Fortaleza"));
+			
+		System.out.println(dataAnalysis.getResultStringNamesAndAge(values));
+		String result3 = dataAnalysis.getResultStringNamesAndAge(values);
+		assertTrue(result3.contains("The first name is smaller or equals in size to the last name and the participant is 30 or younger"));
+		}
+		
+		@Test
+		public void testAnalysis() {
+			
+			DataAnalysis dataAnalysis = new DataAnalysis();
 		ArrayList<String> secondValues = new ArrayList<String>();
 		secondValues.addAll(Arrays.asList("Test Game", "Rafae", "Silva", "F", "31", "Goa"));
 		//System.out.println(dataAnalysis.buildFinalString(secondValues)); 
-		String result3 = dataAnalysis.buildFinalString(secondValues);
-		String result4 = dataAnalysis.getResulStringHomecityAndAge(secondValues);
-		System.out.println(result4);
-		assertTrue(result4.contains("The name of the homecity is small and the participant is 30 or older"));
-		//System.out.println(dataAnalysis.getResulStringHomecityAndAge(values));
+		String result4 = dataAnalysis.buildFinalString(secondValues);
+		}
 		
+		@Test
+		public void testData() {
+			
+			DataAnalysis dataAnalysis = new DataAnalysis();
+			ArrayList<String> secondValues = new ArrayList<String>();
+			secondValues.addAll(Arrays.asList("Test Game", "Rafae", "Silva", "F", "31", "Goa"));
+			
+		String result5 = dataAnalysis.getResulStringHomecityAndAge(secondValues);
+		System.out.println(result5);
+		assertTrue(result5.contains("The name of the homecity is small and the participant is 30 or older"));
+		//System.out.println(dataAnalysis.getResulStringHomecityAndAge(values));
+		}
+		
+		@Test
+		public void testnewData() {
+			
+			DataAnalysis dataAnalysis = new DataAnalysis();
 		ArrayList<String> thirdValues = new ArrayList<String>();
+		
 		thirdValues.addAll(Arrays.asList("Test Game", "Rafael", "Silva", "F", "29", "Goa"));
 		String result6 = dataAnalysis.getResulStringHomecityAndAge(thirdValues);
 		assertTrue(result6.contains("The name of the homecity is small and the participant is younger than 30"));
+		}
 		
+		@Test
+		public void testArrayData() {
+			
+			DataAnalysis dataAnalysis = new DataAnalysis();
 		ArrayList<String> fourthValues = new ArrayList<String>();
+		
 		fourthValues.addAll(Arrays.asList("Test Game", "Rafael", "Silva", "F", "31", "Stockholm"));
 		String result7 = dataAnalysis.getResulStringHomecityAndAge(fourthValues);
 		System.out.println(result7);
 		assertTrue(result7.contains("The name of the homecity is big and the participant is 30 or older"));
-		String result8 = dataAnalysis.getResultStringNamesAndAge(fourthValues);
+		
+		}
+		
+		@Test
+		public void testFourthData() {
+			
+			DataAnalysis dataAnalysis = new DataAnalysis();
+		ArrayList<String> fifthValues = new ArrayList<String>();
+		
+		fifthValues.addAll(Arrays.asList("Test Game", "Rafael", "Silva", "F", "31", "Sweden"));
+		
+		String result8 = dataAnalysis.getResultStringNamesAndAge(fifthValues);
 		assertTrue(result8.contains("The first name is greater or equals in size to the last name and the participant older than 30"));
 	}
 
 }
-/*Game game = new Game();
-		if (values.get(5).length()<5){
-			if (game.getAgeFromInputValues(values)>=30){
-				return "The name of the homecity is small and the participant is 30 or older";
-			}else{
-				return "The name of the homecity is small and the participant is younger than 30";
-			}
-		}else{
-			
-			if (game.getAgeFromInputValues(values)>=30){
-				return "The name of the homecity is big and the participant is 30 or older";
-			}else{
-				return "The name of the homecity is big and the participant is younger than 30";
-				
-			}
-			
-*/
+
--- src/test/java/se/nackademin/examination/examination_jacoco/GameTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/GameTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class GameTest {
 
 	@Test
-	public void testMethod1() {
+	public void testRunGame() {
 		
 		Game game = new Game();
 		ResultFromInputs resultFromInputs = new ResultFromInputs();
@@ -19,19 +19,41 @@ public class GameTest {
 		game.buildFinalString("Tom", "Jerry", resultFromInputs, conversor);
 		ArrayList<String> testValues = new ArrayList<String>();	
 		//testValues = game.collectInputData();
-		
 		game.runGame("Test", "Tom", "Jerry", 'F', 20, "stockholm");
-		
+	}
+	
+	@Test
+	public void testGameRun()
+	{
+		Game game = new Game();
 		ArrayList<String> valuesList = new ArrayList<String>(); 
 		valuesList.addAll(Arrays.asList("Game", "Rafae", "Silva", "M", "29", "Fortaleza"));
 		game.run(valuesList);
-		
-		int nameValue = game.calculateOutPutBasedOnNames("Rafael", "Silva");
-		assertEquals("The result should be 1", nameValue, 1);
-		
-		nameValue = game.calculateOutPutBasedOnNames("Raj", "Saritha");
-		assertEquals("The result should be 0", nameValue, 0);
-		
+	}
+	
+	@Test
+	public void testOutputBasedonAge()
+	{
+		Game game = new Game();
+		int valueAge = game.calculateOutPutBasedOnAge(20);
+		assertEquals("The result should be 20", valueAge, 0);
+	}
+	
+	@Test
+	public void testOutputBasedonGender()
+	{
+		Game game = new Game();
+		int valueGender = game.calculateOutPutBasedOnGender('M');
+		assertEquals("The result should be 0",valueGender , 0);
+		valueGender = game.calculateOutPutBasedOnGender('F');
+		assertEquals("The result should be 1",valueGender , 1);
+		valueGender = game.calculateOutPutBasedOnGender('R');
+		assertEquals("The result should be 1",valueGender , 2);	
+	}
+	@Test
+	public void testCalculationBasedonCity()
+	{
+		Game game = new Game();
 		int valueHomecity = game.calculateOutPutBasedOnHomeCity("Stockholm");
 		assertEquals("The result should be 10", valueHomecity, 10);
 		valueHomecity = game.calculateOutPutBasedOnHomeCity("Atlanta");
@@ -55,31 +77,35 @@ public class GameTest {
 		valueHomecity = game.calculateOutPutBasedOnHomeCity("Johanneshov");
 		assertEquals("The result should be 9", valueHomecity, 9);
 		
-		
-		int valueAge = game.calculateOutPutBasedOnAge(20);
-		assertEquals("The result should be 20", valueAge, 0);
-		
-		int valueGender = game.calculateOutPutBasedOnGender('M');
-		assertEquals("The result should be 0",valueGender , 0);
-		valueGender = game.calculateOutPutBasedOnGender('F');
-		assertEquals("The result should be 1",valueGender , 1);
-		valueGender = game.calculateOutPutBasedOnGender('R');
-		assertEquals("The result should be 1",valueGender , 2);
-	
-		
-	
+	}
+	@Test
+	public void testGetGenderFromInputValues()
+	{
+		Game game = new Game();
 		ArrayList<String> values = new ArrayList<String>();
 		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "30", "Fortaleza"));
 		char genderValue = game.getGenderFromInputValues(values);
 		assertEquals("The result should be M", genderValue, 'M');
-		
+	}
+	
+	@Test
+	public void testCalculateOutPutBasedOnNames()
+	{
+		Game game = new Game();
+		int nameValue = game.calculateOutPutBasedOnNames("Rafael", "Silva");
+		assertEquals("The result should be 1", nameValue, 1);
+		nameValue = game.calculateOutPutBasedOnNames("Raj", "Saritha");
+		assertEquals("The result should be 0", nameValue, 0);
+	}
+	@Test
+	public void testGetAgeFromInputValues()
+	{
+		Game game = new Game();
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "30", "Fortaleza"));
 		int valueAgefromList =game.getAgeFromInputValues(values);
 		assertEquals("The result should be M", valueAgefromList, 30);
-		
-		
 	}
-
-	
 	
 }
 
